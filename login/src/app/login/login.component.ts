6d0477c89e7a2a10575be96b0b699cd72e4a3257
sreeprakash19 <sreeprakash1995@gmail.com>
2019-11-19 11:36:02 +0530

    login page top dwon image working

--- login/src/app/login/login.component.ts
+++ login/src/app/login/login.component.ts
@@ -26,18 +26,25 @@ import { transition, trigger, query, style, animate, stagger,AnimationEvent  } f
             animate('1500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'translateY(10%)'}))
           ])
         ], { optional: true }),
-
+//image1
          query('.img1', [
           style({opacity: 0, transform: 'translateX(-50%)'}),
-          stagger('700ms', [
+          stagger('50ms', [
             animate('1000ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1,transform: 'none' }))
           ])
         ], { optional: true }),
-
+//image2
            query('.img2', [
-          style({opacity: 0, transform: 'translateX(50%)'}),
+          style({opacity: 0, transform: 'translateX(-50%)'}),
           stagger('50ms', [
             animate('900ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1,transform: 'none' }))
+          ])
+        ], { optional: true }),
+//image3
+          query('.img3', [
+          style({opacity: 0, transform: 'translateX(-50%)'}),
+          stagger('50ms', [
+            animate('1000ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1,transform: 'none' }))
           ])
         ], { optional: true }),
   
